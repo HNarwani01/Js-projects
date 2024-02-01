76184e74343b7d866d73395d9765108167702615
HNarwani01 <himanshurawat0075407@gmail.com>
2024-02-02 00:34:38 +0530

    data added into js file in JSON format

---
+++ Card-Flip/js/script.js
@@ -0,0 +1,54 @@
+const data=[
+    {
+        id: 1,
+        image:'../img/01.jpg'
+    },
+    {
+        id: 2,
+        image:'../img/02.jpg'
+    },
+    {
+        id: 3,
+        image:'../img/03.jpg'
+    },
+    {
+        id: 4,
+        image:'../img/04.jpg'
+    },
+    {
+        id: 5,
+        image:'../img/05.jpg'
+    },
+    {
+        id: 6,
+        image:'../img/06.jpg'
+    },
+    {
+        id: 7,
+        image:'../img/07.jpg'
+    },
+    {
+        id: 8,
+        image:'../img/08.jpg'
+    },
+    {
+        id: 9,
+        image:'../img/09.jpg'
+    },
+    {
+        id:10,
+        image:'../img/10.jpg'
+    },
+    {
+        id:11,
+        image:'../img/11.jpg'
+    },
+    {
+        id:12,
+        image:'../img/12.jpg'
+    },
+    {
+        id:13,
+        image:'../img/13.jpg'
+    },
+]
