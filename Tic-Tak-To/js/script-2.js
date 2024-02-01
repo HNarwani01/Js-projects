682b88c24da8a313d4a59a2e1507facf09cfe245
HNarwani01 <himanshurawat0075407@gmail.com>
2024-02-01 23:20:29 +0530

    adding score feature

--- Tic-Tak-To/js/script-2.js
+++ Tic-Tak-To/js/script-2.js
@@ -1,6 +1,11 @@
 const ticTakToe = {
-    turnCheck:'X' ,
-    toBecontinued:true,
+    turnCheck: 'X',
+    toBecontinued: true,
+    score: {
+        winX: 0,
+        winO: 0,
+        tie: 0
+    },
     divbox: document.querySelectorAll('.input'),
     winarray: [
         [0, 1, 2],
@@ -12,46 +17,77 @@ const ticTakToe = {
         [0, 4, 8],
         [2, 4, 6]
     ],
-    init:function () {
-        this.divbox.forEach(value=>{
-            value.addEventListener('click', ()=>{
-                    if (!this.toBecontinued) {
-                        return;
-                    }  
-                    let dataValue =  value.dataset.id;
-                    let targetdiv = document.querySelector(`#js-input-${dataValue}`);
-                    this.renderMove(targetdiv);
-                    
-                });
-                
-            })
+    init: function () {
+        this.divbox.forEach(value => {
+            value.addEventListener('click', () => {
+                if (!this.toBecontinued) {
+                    return;
+                }
+                let dataValue = value.dataset.id;
+                let targetdiv = document.querySelector(`#js-input-${dataValue}`);
+                this.renderMove(targetdiv);
+
+            });
+
+        })
     },
-    renderMove:function (targetdiv) {
-        if (targetdiv.innerHTML==='') {
+    renderMove: function (targetdiv) {
+        if (targetdiv.innerHTML === '') {
             targetdiv.innerHTML = this.turnCheck;
-            let value = (this.turnCheck==='X')?'O':'X';
+            let value = (this.turnCheck === 'X') ? 'O' : 'X';
             this.turnCheck = value;
             this.checkWinner()
             this.showWinner()
         } else {
             alert('this block is already filled')
-        }    
+        }
     },
-    checkWinner:function () {
+    checkWinner: function () {
         let yourmove = [];
-        this.divbox.forEach(val=>{
+        this.divbox.forEach(val => {
             yourmove.push(val.innerHTML)
         })
-        this.winarray.forEach(arr=>{
-            if (yourmove[arr[0]]===yourmove[arr[1]]&&yourmove[arr[1]]===yourmove[arr[2]]&&yourmove[arr[0]]!=="") {
+        this.winarray.forEach(arr => {
+            if (yourmove[arr[0]] === yourmove[arr[1]] && yourmove[arr[1]] === yourmove[arr[2]] && yourmove[arr[0]] !== "") {
                 this.toBecontinued = false
             }
         })
     },
-    showWinner:function () {
+    showWinner: function () {
         if (!this.toBecontinued) {
-            document.querySelector('.result').innerHTML = (`${(this.turnCheck==='O')?'X':'O'} wins`)
+            let winner = (this.turnCheck === 'O') ? 'X' : 'O';
+            document.querySelector('.winner').innerHTML = (`${winner} wins`)
+            this.renderScore(winner);
         }
+        
+    },
+    renderScore:function (winner) {
+        if (winner==='X') {
+            this.score.winX ++;
+            document.querySelector('.result-X').innerHTML=this.score.winX;
+        } else if(winner==='O'){
+            this.score.winO ++;
+            document.querySelector('.result-O').innerHTML=this.score.winX;
+        }else{
+
+        }
+    },
+    resetScore: function () {
+        this.score.winX = 0;
+        this.score.tie = 0;
+        this.score.winO = 0;
+        this.divbox.forEach(val=>{
+            val.innerHTML ='';
+        })
+        this.toBecontinued = true;
+        this.turnCheck = 'X'
+    },
+    playAgain:function () {
+        this.divbox.forEach(val=>{
+            val.innerHTML ='';
+        })
+        this.toBecontinued = true;
+        this.turnCheck = 'X'
     }
 }
 
